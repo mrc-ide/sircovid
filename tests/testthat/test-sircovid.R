4e421ec5c0d6d780a413d923f4e9aa3e23d6c98f
John Lees <lees.john6@gmail.com>
2020-04-06 09:59:51 +0100

    Slight improvement to run_particle_filter tests

--- tests/testthat/test-sircovid.R
+++ tests/testthat/test-sircovid.R
@@ -1,6 +1,7 @@
 context("sircovid")
 
 test_that("Can be run on real data", {
+  set.seed(1)
   time_steps_per_day <- 4
   
   vary_beta <- generate_beta(0.1)
@@ -18,8 +19,10 @@ test_that("Can be run on real data", {
                                                    exp_noise = 1e6),
                                  n_particles = 1000)
   # No check of correctness  
-  expect_lt(results$log_likelihood, 0)
-  
+  expect_equal(results$log_likelihood, -14074.79, tolerance=1e-3)
+})
+
+test_that("Poor formatting of real data errors", {
   # Try some bad files and dates
   expect_error(run_particle_filter(case_data_file = "covid_cases_2020_4_3.csv",
                                    admissions_data_file = "combin_time_series.csv",
@@ -43,5 +46,17 @@ test_that("Can be run on real data", {
                                                      exp_noise = 1e6),
                                    n_particles = 1000),
                "No case data matching country and date filtering criteria")
+  expect_error(run_particle_filter(case_data_file = "covid_cases_2020_4_3.csv",
+                                   admissions_data_file = "combin_time_series.csv",
+                                   model_params = model_params,
+                                   first_data_date = "2020-01-01",
+                                   model_start_date = "2020-02-02",
+                                   obs_params = list(phi_ICU = 0.95,
+                                                     k_ICU = 2,
+                                                     phi_death = 1789/1651,
+                                                     k_death = 2,
+                                                     exp_noise = 1e6),
+                                   n_particles = 1000),
+               "Model start date is later than data start date")
 })
 
