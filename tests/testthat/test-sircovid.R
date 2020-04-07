9f9718015d5ab69fbc1107a56e11026838627c55
John Lees <lees.john6@gmail.com>
2020-04-07 12:26:42 +0100

    Fix sircovid tests

--- tests/testthat/test-sircovid.R
+++ tests/testthat/test-sircovid.R
@@ -24,7 +24,15 @@ test_that("Can be run on real data", {
   expect_equal(results$log_likelihood, -269.9478, tolerance=1e-3)
 })
 
-test_that("Poor formatting of real data throwserrors", {
+test_that("Poor formatting of real data throws errors", {
+  data <- generate_data(death_data_file = "covid_cases_2020_4_3.csv",
+                        admissions_data_file = "combin_time_series.csv")
+  
+  vary_beta <- generate_beta(0.1)
+  model_params <- generate_parameters(beta = vary_beta$beta,
+                                      beta_times = vary_beta$beta_times,
+                                      dt = 0.25)
+  
   # Try some bad files and dates
   expect_error(generate_data(death_data_file = "covid_cases_2020_4_3.csv",
                              admissions_data_file = "combin_time_series.csv",
@@ -35,11 +43,9 @@ test_that("Poor formatting of real data throwserrors", {
                              first_data_date = "2022-01-01"),
                "No case data matching country and date filtering criteria")
   
-  data <- generate_data(death_data_file = "covid_cases_2020_4_3.csv",
-                        admissions_data_file = "combin_time_series.csv",
-                        first_data_date = "2020-01-01")
   expect_error(run_particle_filter(data = data,
                                    model_start_date = "2020-03-02",
+                                   model_params = model_params,
                                    obs_params = list(phi_ICU = 0.95,
                                                      k_ICU = 2,
                                                      phi_death = 1789/1651,
@@ -51,10 +57,15 @@ test_that("Poor formatting of real data throwserrors", {
 
 test_that("Bad parameters throws errors", {
   data <- generate_data(death_data_file = "covid_cases_2020_4_3.csv",
-                        admissions_data_file = "combin_time_series.csv",
-                        first_data_date = "2020-03-01")
+                        admissions_data_file = "combin_time_series.csv")
+  
+  vary_beta <- generate_beta(0.1)
+  model_params <- generate_parameters(beta = vary_beta$beta,
+                                      beta_times = vary_beta$beta_times,
+                                      dt = 0.25)
   
   expect_message(run_particle_filter(data = data,
+                                     model_params = model_params,
                                    model_start_date = "2020-02-02",
                                    obs_params = list(phi_ICU = 0.95,
                                                      k_ICU = 2,
@@ -66,6 +77,7 @@ test_that("Bad parameters throws errors", {
                                    return = "sample"),
                "Must save particles to sample runs")
   expect_error(run_particle_filter(data = data,
+                                   model_params = model_params,
                                      model_start_date = "2020-02-02",
                                      obs_params = list(phi_ICU = 0.95,
                                                        k_ICU = 2,
