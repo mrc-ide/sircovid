b316b7b89b162e46c06a8066276c19e62b7e3026
edknock <e.knock@imperial.ac.uk>
2020-04-16 20:17:19 +0100

    tests for hospital model using new model object setup

--- tests/testthat/test-hospital-model.R
+++ tests/testthat/test-hospital-model.R
@@ -3,35 +3,40 @@ context("new hospital model for covid transmission")
 ## Sanity Checks
 
 test_that("there are no infections when beta is 0", {
-
-    pars_model <- generate_parameters_new_hospital_model(beta = 0, beta_times = "2020-02-01")
-    mod <- new_hospital_model(user = pars_model)
+    
+    sircovid_model <- hospital_model()
+    pars_model <- generate_parameters(sircovid_model = sircovid_model, beta = 0)
+    mod <- sircovid_model$odin_model(user = pars_model)
     t_max <- 150
     t <- seq(from = 1, to = t_max)
-    tmp <- mod$run(t, replicate = 1)
+    tmp <- mod$run(t)
     results <- mod$transform_variables(tmp)
      #should be TRUE
-    expect_true(all(results$S[t_max,,1] == pars_model$S0))
+    expect_true(all(results$S[t_max,] == pars_model$S0))
 
   }
 )
 
 test_that("everyone is infected when beta is Inf", {
-    pars_model <- generate_parameters_new_hospital_model(beta = 1e100, beta_times = "2020-02-01")
-    mod <- new_hospital_model(user = pars_model)
+  
+    sircovid_model <- hospital_model()
+    pars_model <- generate_parameters(sircovid_model = sircovid_model, beta = Inf)
+    mod <- sircovid_model$odin_model(user = pars_model)
     t_max <- 150
     t <- seq(from = 1, to = t_max)
-    tmp <- mod$run(t, replicate = 1)
+    tmp <- mod$run(t)
     results <- mod$transform_variables(tmp)
      #should be TRUE
-    expect_true(all(results$S[2:t_max,,1] == 0))
+    expect_true(all(results$S[2:t_max,] == 0))
  }
 
 )
 
 
 test_that("No one is infected if I and E are 0 at t = 0", {
-    pars_model <- generate_parameters_new_hospital_model(beta = 0.042, beta_times = "2020-02-01")
+  
+    sircovid_model <- hospital_model()
+    pars_model <- generate_parameters(sircovid_model = sircovid_model, beta = 0.042)
     pars_model$E0[,,]<- 0
     pars_model$I0_asympt[,,]<- 0
     pars_model$I0_mild[,,]<- 0
@@ -42,20 +47,22 @@ test_that("No one is infected if I and E are 0 at t = 0", {
     pars_model$I0_ICU_D[,,] <- 0
     pars_model$I0_triage[,,] <- 0
 
-    mod <- new_hospital_model(user = pars_model)
+    mod <- sircovid_model$odin_model(user = pars_model)
     t_max <- 150
     t <- seq(from = 1, to = t_max)
-    tmp <- mod$run(t, replicate = 1)
+    tmp <- mod$run(t)
     results <- mod$transform_variables(tmp)
-    expect_true(all(results$S[t_max,,1] == pars_model$S0))
+    expect_true(all(results$S[t_max,] == pars_model$S0))
 
  }
 )
 
 test_that("No one is hospitalised if p_sympt_ILI is 0", {
-    pars_model <- generate_parameters_new_hospital_model(beta = 0.042, beta_times = "2020-02-01")
+    
+    sircovid_model <- hospital_model()
+    pars_model <- generate_parameters(sircovid_model = sircovid_model, beta = 0.042)
     pars_model$p_sympt_ILI[]<-0
-    mod <- new_hospital_model(user = pars_model)
+    mod <- sircovid_model$odin_model(user = pars_model)
     t_max <- 150
     t <- seq(from = 1, to = t_max)
     check_cases <- FALSE
@@ -63,51 +70,51 @@ test_that("No one is hospitalised if p_sympt_ILI is 0", {
     iter <- 0
     while (!check_cases && iter<= max_iter){
       iter <- iter + 1
-      tmp <- mod$run(t, replicate = 1)
+      tmp <- mod$run(t)
       results <- mod$transform_variables(tmp)
-      if (any(results$E[,,,,1] > 0)){
+      if (any(results$E[,,,] > 0)){
         check_cases <- TRUE
       }
     }
     #check there were people infected (if yes, should be TRUE)
-    expect_true(all(results$I_ILI[,,,,1] == 0))
-    expect_true(all(results$I_hosp_R[,,,,1] == 0))
-    expect_true(all(results$I_hosp_D[,,,,1] == 0))
-    expect_true(all(results$I_ICU_R[,,,,1] == 0))
-    expect_true(all(results$I_ICU_D[,,,,1] == 0))
-    expect_true(all(results$I_triage[,,,,1] == 0))
-    expect_true(all(results$R_stepdown[,,,,1] == 0))
-    expect_true(all(results$D[,,1]==0))
+    expect_true(all(results$I_ILI[,,,] == 0))
+    expect_true(all(results$I_hosp_R[,,,] == 0))
+    expect_true(all(results$I_hosp_D[,,,] == 0))
+    expect_true(all(results$I_ICU_R[,,,] == 0))
+    expect_true(all(results$I_ICU_D[,,,] == 0))
+    expect_true(all(results$I_triage[,,,] == 0))
+    expect_true(all(results$R_stepdown[,,,] == 0))
+    expect_true(all(results$D[,]==0))
  }
 )
 
 
 test_that("No one is hospitalised if p_recov_ILI is 0", {
+  #p_recov_ILI=1, no-one hospitalised
+  sircovid_model <- hospital_model()
+  pars_model <- generate_parameters(sircovid_model = sircovid_model, beta = 0.126)
+  pars_model$p_recov_ILI[]<-1
+  mod <- sircovid_model$odin_model(user = pars_model)
   t_max <- 150
   t <- seq(from = 1, to = t_max)
-  max_iter <- 10
-  
-  #p_recov_ILI=1, no-one hospitalised
-  pars_model <- generate_parameters_new_hospital_model(beta = 0.126, beta_times = "2020-02-01")
-  pars_model$p_recov_ILI[]<-1
-  mod <- new_hospital_model(user = pars_model)
   check_cases <- FALSE
+  max_iter <- 10
   iter <- 0
   while (!check_cases && iter<= max_iter){
     iter <- iter + 1
-    tmp <- mod$run(t, replicate = 1)
+    tmp <- mod$run(t)
     results <- mod$transform_variables(tmp)
-    if (any(results$I_ILI[,,,,1] > 0)){
+    if (any(results$I_ILI[,,,] > 0)){
       check_cases <- TRUE
     }
   }
-  expect_true(all(results$I_hosp_R[,,,,1] == 0))
-  expect_true(all(results$I_hosp_D[,,,,1] == 0))
-  expect_true(all(results$I_ICU_R[,,,,1] == 0))
-  expect_true(all(results$I_ICU_D[,,,,1] == 0))
-  expect_true(all(results$I_triage[,,,,1] == 0))
-  expect_true(all(results$R_stepdown[,,,,1] == 0))
-  expect_true(all(results$D[,,1]==0))
+  expect_true(all(results$I_hosp_R[,,,] == 0))
+  expect_true(all(results$I_hosp_D[,,,] == 0))
+  expect_true(all(results$I_ICU_R[,,,] == 0))
+  expect_true(all(results$I_ICU_D[,,,] == 0))
+  expect_true(all(results$I_triage[,,,] == 0))
+  expect_true(all(results$R_stepdown[,,,] == 0))
+  expect_true(all(results$D[,]==0))
 }
 )
 
@@ -116,7 +123,9 @@ test_that("setting hospital route probabilities to 0 or 1 result in correct path
   t_max <- 150
   t <- seq(from = 1, to = t_max)
   max_iter <- 10
-
+  
+  sircovid_model <- hospital_model()
+  
   check_hosp_probs <- function(
     p_ICU_hosp,
     p_death_ICU = NULL,
@@ -124,7 +133,7 @@ test_that("setting hospital route probabilities to 0 or 1 result in correct path
     cases, #name of comparment where we expect cases
     zeroes #names of compartmens where we expect zeroes
     ){
-    pars_model <- generate_parameters_new_hospital_model(beta = 0.126, beta_times = "2020-02-01")
+    pars_model <- generate_parameters(sircovid_model = sircovid_model, beta = 0.126)
     pars_model$p_ICU_hosp[] <- p_ICU_hosp
     if (!is.null(p_death_hosp)){
       pars_model$p_death_hosp[] <- p_death_hosp
@@ -132,12 +141,12 @@ test_that("setting hospital route probabilities to 0 or 1 result in correct path
     if (!is.null(p_death_ICU)){
       pars_model$p_death_ICU[] <- p_death_ICU
     }
-    mod <- new_hospital_model(user = pars_model)
+    mod <- sircovid_model$odin_model(user = pars_model)
     check_cases <- FALSE
     iter <- 0
-    while (!check_cases && iter<= max_iter){
+    while (!check_cases && iter<=max_iter){
       iter <- iter + 1
-      tmp <- mod$run(t, replicate = 1)
+      tmp <- mod$run(t)
       results <- mod$transform_variables(tmp)
       if (any(results[[cases]] > 0)){
         check_cases <- TRUE
@@ -178,29 +187,30 @@ test_that("setting hospital route probabilities to 0 or 1 result in correct path
 })
 
 
-test_that("setting a gamma to Inf or 1 results in progress in corresponding compartment in 1 time-step", {    
+test_that("setting a gamma to Inf results in progress in corresponding compartment in 1 time-step", {    
   t_max <- 150
   t <- seq(from = 1, to = t_max)
   max_iter <- 10
   
+  sircovid_model <- hospital_model(use_fitted_parameters = FALSE ,progression_groups = list(E = 2, asympt = 2, mild = 2, ILI = 2, hosp_D = 2 , hosp_R = 2, ICU_D = 2, ICU_R = 2, triage = 2, stepdown = 2))
+  
   #function to test that setting a given gamma to Inf causes cases in corresponding compartment to
   #progress in 1 time-step
   test_gamma_inf <- function(gamma_name,compartment_name){
-    pars_model <- generate_parameters_new_hospital_model(beta = 0.126, beta_times = "2020-02-01",
-                                                         progression_groups = list(E = 2, asympt = 2, mild = 2, ILI = 2, hosp_D = 2 , hosp_R = 2, ICU_D = 2, ICU_R = 2, triage = 2, stepdown = 2))
+    pars_model <- generate_parameters(sircovid_model = sircovid_model, beta = 0.126)
     pars_model[gamma_name] <- Inf
-    mod <- new_hospital_model(user = pars_model)
+    mod <- sircovid_model$odin_model(user = pars_model)
     check_cases <- FALSE
     iter <- 0
     while (!check_cases && iter<= max_iter){
       iter <- iter + 1
-      tmp <- mod$run(t, replicate = 1)
+      tmp <- mod$run(t)
       results <- mod$transform_variables(tmp)
-      if (any(results[[compartment_name]][,,,,1] > 0)){
+      if (any(results[[compartment_name]][,,,] > 0)){
         check_cases <- TRUE
       }
     }
-    expect_true(all(results[[compartment_name]][2:t_max,,2,,]==results[[compartment_name]][1:(t_max-1),,1,,]))
+    expect_true(all(results[[compartment_name]][2:t_max,,2,]==results[[compartment_name]][1:(t_max-1),,1,]))
   }
   
   test_gamma_inf('gamma_E','E')
@@ -220,24 +230,25 @@ test_that("setting a gamma to 0 results in cases in corresponding compartment to
   t <- seq(from = 1, to = t_max)
   max_iter <- 10
   
+  sircovid_model <- hospital_model(use_fitted_parameters = FALSE, progression_groups = list(E = 2, asympt = 2, mild = 2, ILI = 2, hosp_D = 2 , hosp_R = 2, ICU_D = 2, ICU_R = 2, triage = 2, stepdown = 2))
+  
   #function to test that setting a given gamma to 0 causes cases in corresponding compartment to
   #stay in progression stage 1
   test_gamma_zero <- function(gamma_name,compartment_name){
-    pars_model <- generate_parameters_new_hospital_model(beta = 0.126, beta_times = "2020-02-01",
-                                                         progression_groups = list(E = 2, asympt = 2, mild = 2, ILI = 2, hosp_D = 2 , hosp_R = 2, ICU_D = 2, ICU_R = 2, triage = 2, stepdown = 2))
+    pars_model <- generate_parameters(sircovid_model = sircovid_model, beta = 0.126)
     pars_model[gamma_name] <- 0
-    mod <- new_hospital_model(user = pars_model)
+    mod <- sircovid_model$odin_model(user = pars_model)
     check_cases <- FALSE
     iter <- 0
     while (!check_cases && iter<= max_iter){
       iter <- iter + 1
-      tmp <- mod$run(t, replicate = 1)
+      tmp <- mod$run(t)
       results <- mod$transform_variables(tmp)
-      if (any(results[[compartment_name]][,,,,1] > 0)){
+      if (any(results[[compartment_name]][,,,] > 0)){
         check_cases <- TRUE
       }
     }
-    expect_true(all(results[[compartment_name]][,,2,,]==0))
+    expect_true(all(results[[compartment_name]][,,2,]==0))
   }
   
   test_gamma_zero('gamma_E','E')
