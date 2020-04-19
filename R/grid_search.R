595f9c535342d3efe88c49743226032b90f84759
edknock <e.knock@imperial.ac.uk>
2020-04-19 10:49:06 +0100

    first version of 3d grid

--- R/grid_search.R
+++ R/grid_search.R
@@ -121,6 +121,139 @@ scan_beta_date <- function(
   results
 }
 
+##' Run a grid search of the particle filter over beta, start date and
+##' beta_reduction. This is parallelised, first run \code{plan(multiprocess)}
+##' to set this up.
+##' 
+##' @title Grid search of beta and start date
+##' 
+##' @param min_beta Minimum value of beta in the search
+##' 
+##' @param max_beta Maximum value of beta in the search
+##' 
+##' @param beta_step Step to increment beta between min and max
+##' 
+##' @param first_start_date Earliest start date as 'yyyy-mm-dd'
+##' 
+##' @param last_start_date Latest start date as 'yyyy-mm-dd'
+##' 
+##' @param day_step Step to increment date in days
+##' 
+##' @param min_beta_reduction Minimum value of beta_reduction in the search
+##' 
+##' @param max_beta_reduction Maximum value of beta_reduction in the search
+##' 
+##' @param beta_reduction_step Step to increment beta_reduction between min and max
+##' 
+##' @param data Hosptial data to fit to. See \code{example.csv}
+##'   and \code{particle_filter_data()}
+##' 
+##' @param sircovid_model An odin model generator and comparison function.
+##'
+##' @param model_params Model parameters, from a call to
+##'   \code{generate_parameters()}. If NULL, uses defaults as
+##'   in unit tests.
+##'
+##' @param pars_obs list of parameters to use for the comparison function.
+##'   
+##' @param n_particles Number of particles. Positive Integer. Default = 100
+##' 
+##' @return List of beta and start date grid values, and
+##'   normalised probabilities at each point
+##' 
+##' @export
+##' @import furrr
+scan_beta_date_reduction <- function(
+  min_beta, 
+  max_beta, 
+  beta_step,
+  first_start_date, 
+  last_start_date, 
+  day_step,
+  min_beta_reduction, 
+  max_beta_reduction, 
+  beta_reduction_step,
+  data,
+  sircovid_model = basic_model(),
+  model_params = NULL,
+  pars_obs = NULL,
+  n_particles = 100) {
+  #
+  # Set up parameter space to scan
+  #
+  beta_1D <- seq(min_beta, max_beta, beta_step)
+  date_list <- seq(as.Date(first_start_date), as.Date(last_start_date), day_step)
+  beta_reduction_1D <- seq(min_beta_reduction, max_beta_reduction, beta_reduction_step)
+  param_grid <- expand.grid(beta = beta_1D, start_date = date_list, beta_reduction = beta_reduction_1D)
+  
+  #
+  # Set up calls to simulator runs
+  #
+  if (is.null(model_params)) {
+    time_steps_per_day <- 4
+    model_params <- generate_parameters(
+      sircovid_model = sircovid_model,
+      transmission_model = "POLYMOD",
+      beta = 0.1,
+      beta_times = "2020-01-01",
+      trans_profile = 1,
+      trans_increase = 1,
+      dt = 1/time_steps_per_day
+    )
+  } else {
+    if (length(model_params$beta_y) > 1) {
+      stop("Set beta variation through generate_beta_func in sircovid_model, not model_params")
+    }
+  }
+  
+  if (is.null(pars_obs)) {
+    pars_obs <- list(phi_general = 0.95,
+                     k_general = 2,
+                     phi_ICU = 0.95,
+                     k_ICU = 2,
+                     phi_death = 926 / 1019,
+                     k_death = 2,
+                     exp_noise = 1e6)
+  }
+  
+  #
+  # Multi-core futures with furrr (parallel purrr)
+  #
+  ## Particle filter outputs, extracting log-likelihoods
+  pf_run_ll <- furrr::future_pmap_dbl(
+    .l = param_grid, .f = beta_date_reduction_particle_filter,
+    sircovid_model = sircovid_model, model_params = model_params, data = data, 
+    pars_obs = pars_obs, n_particles = n_particles,
+    forecast_days = 0, save_particles = FALSE, return = "ll"
+  )
+  
+  ## Construct a matrix with start_date as columns, and beta as rows
+  ## order of return is set by order passed to expand.grid, above
+  ## Returned column-major (down columns of varying beta) - set byrow = FALSE
+  mat_log_ll <- array(
+    pf_run_ll, 
+    dim = c(length(beta_1D), length(date_list), length(beta_reduction_1D))
+    )
+  
+  # Exponentiate elements and normalise to 1 to get probabilities
+  prob_matrix <- exp(mat_log_ll)
+  renorm_mat_LL <- prob_matrix/sum(prob_matrix)
+  
+  results <- list(x = beta_1D, 
+                  y = date_list,
+                  z = beta_reduction_1D,
+                  mat_log_ll = mat_log_ll,
+                  renorm_mat_LL = renorm_mat_LL,
+                  inputs = list(
+                    model = sircovid_model,
+                    model_params = model_params,
+                    pars_obs = pars_obs,
+                    data = data))
+  
+  class(results) <- "sircovid_scan_3D"
+  results
+}
+
 ##' @export
 plot.sircovid_scan <- function(x, ..., what = "likelihood") {
   if (what == "likelihood") {
@@ -130,6 +263,31 @@ plot.sircovid_scan <- function(x, ..., what = "likelihood") {
     graphics::image(x=x$x, y=x$y, z=x$renorm_mat_LL,
                     xlab="beta", ylab="Start date", main = "Probability")
   }
+}
+
+##' @export
+plot.sircovid_scan_3D <- function(x, ..., integrate_out = "start_date") {
+  if (integrate_out == "beta"){
+    z <- t(apply(x$renorm_mat_LL,c(2,3),sum))
+    xlab = "beta_reduction"
+    ylab = "Start date"
+    xx = x$z
+    yy = x$y
+  } else if (integrate_out == "start_date"){
+    z <- apply(x$renorm_mat_LL,c(1,3),sum)
+    xlab = "beta"
+    ylab = "beta_reduction"
+    xx = x$x
+    yy = x$z
+  } else if (integrate_out == "beta_reduction"){
+    z <- apply(x$renorm_mat_LL,c(1,2),sum)
+    xlab = "beta"
+    ylab = "Start date"
+    xx = x$x
+    yy = x$y
+  }
+  graphics::image(x=xx, y=yy, z=z,
+                    xlab=xlab, ylab=ylab, main = "Probability")
 }
 
 ##' @export
@@ -201,6 +359,33 @@ beta_date_particle_filter <- function(beta, start_date,
   model_params$beta_y <- new_beta$beta
   model_params$beta_t <- beta_t
 
+  X <- run_particle_filter(data, sircovid_model, model_params, start_date, pars_obs,
+                           n_particles, forecast_days, save_particles, return)
+  
+  X
+}
+
+##' Particle filter outputs
+##' 
+##' Helper function to run the particle filter with a
+##' new beta, start date and beta reduction
+##' 
+##' @noRd
+beta_date_reduction_particle_filter <- function(beta, start_date,
+                                                beta_reduction,
+                                                sircovid_model,
+                                                model_params, data, 
+                                                pars_obs, n_particles,
+                                                forecast_days = 0,
+                                                save_particles = FALSE,
+                                                return = "full") {
+  # Edit beta in parameters
+  new_beta <- sircovid_model$generate_beta_func(beta_start = beta, start_date = start_date, beta_reduction = beta_reduction)
+  beta_t <- normalise_beta(new_beta$beta_times, model_params$dt)
+  
+  model_params$beta_y <- new_beta$beta
+  model_params$beta_t <- beta_t
+  
   X <- run_particle_filter(data, sircovid_model, model_params, start_date, pars_obs,
                            n_particles, forecast_days, save_particles, return)
   
--- R/sample_grid.R
+++ R/sample_grid.R
@@ -107,3 +107,113 @@ sample_grid_scan <- function(scan_results,
   return(res)
   
 }
+
+##' Take a grid search produced by \code{\link{scan_beta_date_reduction}} and 
+##' sample \code{n_sample_pairs} from the parameter grid uses based
+##' on their probability. For each parameter pair chosen, run particle
+##' filter with \code{num_particles} and sample 1 trajectory
+##' 
+##' @title Sample Grid Scan
+##' 
+##' @param scan_results Output of \code{\link{scan_beta_date_reduction}}.
+##' 
+##' @param n_sample_pairs Number of parameter pairs to be sampled. This will 
+##'   determine how many trajectories are returned. Integer. Default = 10. This 
+##'   will determine how many trajectories are returned. 
+##'   
+##' @param n_particles Number of particles. Positive Integer. Default = 100
+##' 
+##' @param forecast_days Number of days being forecast. Default = 0
+##'   
+##' @return \code{\link{list}}. First element (trajectories) is a 3 
+##'   dimensional array of trajectories (time, state, tranjectories). Second 
+##'   element (param_grid) is the parameters chosen when  sampling from the 
+##'   \code{scan_results} grid and the third dimension (inputs) is a list of
+##'   model inputs. 
+##' 
+##' @export
+##' @import furrr
+##' @importFrom utils tail
+sample_grid_scan_3d <- function(scan_results,
+                             n_sample_pairs = 10, 
+                             n_particles = 100, 
+                             forecast_days = 0) {
+  
+  # checks on args
+  assert_is(scan_results, "sircovid_scan_3D")
+  assert_pos_int(n_sample_pairs)
+  assert_pos_int(n_particles)
+  assert_pos_int(forecast_days)
+  
+  # grab the pobability matrix
+  prob <- scan_results$renorm_mat_LL
+  nx <- dim(prob)[1]
+  ny <- dim(prob)[2]
+  nz <- dim(prob)[3]
+  
+  scan_param_grid <- expand.grid(beta = scan_results$x,
+                                 start_date = scan_results$y,
+                                 beta_reduction = scan_results$z)
+  
+  # draw which grid pairs are chosen
+  pairs <- sample(x =  length(prob), size = n_sample_pairs, 
+                  replace = TRUE, prob = prob)
+  
+  # what are related beta and dates
+  beta <- scan_param_grid$beta[pairs]
+  dates <- scan_param_grid$start_date[pairs]
+  beta_reduction <- scan_param_grid$beta_reduction[pairs]
+  
+  # recreate parameters for re running
+  param_grid <- data.frame("beta" = beta, "start_date" = dates, "beta_reduction" = beta_reduction)
+  sircovid_model <- scan_results$inputs$model
+  model_params <- scan_results$inputs$model_params
+  pars_obs <- scan_results$inputs$pars_obs
+  data <- scan_results$inputs$data
+  
+  
+  # Multi-core futures with furrr (parallel purrr)
+  
+  ## Particle filter outputs
+  ## Sample one particle
+  traces <- furrr::future_pmap(
+    .l = param_grid, .f = beta_date_reduction_particle_filter,
+    sircovid_model = sircovid_model, 
+    model_params = model_params, data = data,
+    pars_obs = pars_obs, n_particles = n_particles,
+    forecast_days = forecast_days, 
+    save_particles = TRUE, return = "sample")
+  
+  # collapse into an array of trajectories
+  # the trajectories are different lengths in terms of dates
+  # so we will fill the arrays with NAs where needed
+  num_rows <- unlist(lapply(traces, nrow))
+  max_rows <- max(num_rows)
+  seq_max <- seq_len(max_rows)
+  max_date_names <- rownames(traces[[which.max(unlist(lapply(traces, nrow)))]])
+  
+  trajectories <- array(NA, 
+                        dim = c(max_rows, ncol(traces[[1]]), length(traces)),
+                        dimnames = list(max_date_names, NULL, NULL))
+  
+  # fill the tail of the array slice
+  # This is so that the end of the trajectories array is populated, 
+  # and the start is padded with NA if it's shorter than the max. 
+  for (i in seq_len(length(traces))){
+    trajectories[tail(seq_max, nrow(traces[[i]])), , i] <- traces[[i]]
+  }
+  
+  # combine and return
+  res <- list("trajectories" = trajectories,
+              "param_grid" = param_grid,
+              inputs = list(
+                model_params = model_params,
+                pars_obs = pars_obs,
+                data = data,
+                model = sircovid_model))
+  
+  class(res) <- "sample_grid_search"
+  
+  return(res)
+  
+}
