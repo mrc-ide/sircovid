bcdf89447957ee8df5872718b5e38245b9a39f9a
sangeetabhatia03 <sangeetabhatia03@gmail.com>
2020-04-22 21:22:24 +0100

    Add a warning when edges of probability matrix stray away from 0. Added test for warning

--- tests/testthat/test-grid_search.R
+++ tests/testthat/test-grid_search.R
@@ -6,7 +6,7 @@ test_that("Small grid search works", {
 
   data <- read.csv(sircovid_file("extdata/example.csv"),
                    stringsAsFactors = FALSE)
- 
+
   # Parameters for run
   min_beta <- 0.1
   max_beta <- 0.2
@@ -14,22 +14,22 @@ test_that("Small grid search works", {
   first_start_date <- "2020-01-21"
   last_start_date <- "2020-01-22"
   day_step <- 1
-  
+
   scan_results = scan_beta_date(
     min_beta = min_beta,
     max_beta = max_beta,
     beta_step = beta_step,
-    first_start_date = first_start_date, 
-    last_start_date = last_start_date, 
+    first_start_date = first_start_date,
+    last_start_date = last_start_date,
     day_step = day_step,
     data = data,
     sircovid_model = basic_model())
-   
+
   expect_is(scan_results, "sircovid_scan")
   expect_true("inputs" %in% names(scan_results))
   expect_setequal(names(scan_results$inputs),
                   c("model", "model_params", "pars_obs", "data"))
-  
+
   beta_grid <- seq(min_beta, max_beta, beta_step)
   date_grid <- seq(as.Date(first_start_date), as.Date(last_start_date), day_step)
   expect_equal(scan_results$x, beta_grid)
@@ -37,7 +37,7 @@ test_that("Small grid search works", {
   expect_equal(dim(scan_results$renorm_mat_LL), dim(scan_results$mat_log_ll))
   expect_equal(dim(scan_results$renorm_mat_LL), c(length(beta_grid), length(date_grid)))
   expect_true(all(scan_results$renorm_mat_LL <= 1 & scan_results$renorm_mat_LL >= 0))
-  
+
   # Plots run, but not checked
   plot(scan_results)
 })
@@ -46,7 +46,7 @@ test_that("Small grid search works with new model", {
   set.seed(1)
 
   data <- readRDS("hospital_model_data.rds")
- 
+
   # Parameters for run
   min_beta <- 0.1
   max_beta <- 0.2
@@ -54,24 +54,47 @@ test_that("Small grid search works with new model", {
   first_start_date <- "2020-01-21"
   last_start_date <- "2020-01-22"
   day_step <- 1
-  
+
   scan_results = scan_beta_date(
     min_beta = min_beta,
     max_beta = max_beta,
     beta_step = beta_step,
-    first_start_date = first_start_date, 
-    last_start_date = last_start_date, 
+    first_start_date = first_start_date,
+    last_start_date = last_start_date,
     day_step = day_step,
     data = data,
     sircovid_model = hospital_model(),
     scale_prior = 0.003541667,
-    shape_prior = 36)
+    shape_prior = 36
+    )
+
+
+  expect_warning(
+    scan_beta_date(
+    min_beta = min_beta,
+    max_beta = max_beta,
+    beta_step = beta_step,
+    first_start_date = first_start_date,
+    last_start_date = last_start_date,
+    day_step = day_step,
+    data = data,
+    sircovid_model = hospital_model(),
+    scale_prior = 0.003541667,
+    shape_prior = 36,
+    ## 292 0s before a non-zero number when I run stuff.
+    ## Hence I am setting tolerance to be very high.
+    ## In practice, we migh need less.
+    tolerance = 1e-300
+    )
+  )
+
+
 
   expect_is(scan_results, "sircovid_scan")
   expect_true("inputs" %in% names(scan_results))
   expect_setequal(names(scan_results$inputs),
                   c("model", "model_params", "pars_obs", "data"))
-  
+
   beta_grid <- seq(min_beta, max_beta, beta_step)
   date_grid <- seq(as.Date(first_start_date), as.Date(last_start_date), day_step)
   expect_equal(scan_results$x, beta_grid)
@@ -79,7 +102,7 @@ test_that("Small grid search works with new model", {
   expect_equal(dim(scan_results$renorm_mat_LL), dim(scan_results$mat_log_ll))
   expect_equal(dim(scan_results$renorm_mat_LL), c(length(beta_grid), length(date_grid)))
   expect_true(all(scan_results$renorm_mat_LL <= 1 & scan_results$renorm_mat_LL >= 0))
-  
+
   # Plots run, but not checked
   plot(scan_results, what = "likelihood")
   plot(scan_results, what = "probability")
@@ -88,10 +111,10 @@ test_that("Small grid search works with new model", {
 
 test_that("Transmission is more likely", {
   set.seed(1)
-  
+
   data <- read.csv(sircovid_file("extdata/example.csv"),
                    stringsAsFactors = FALSE)
-  
+
   # Parameters for run
   min_beta <- 0
   max_beta <- 0.1
@@ -99,27 +122,27 @@ test_that("Transmission is more likely", {
   first_start_date <- "2020-01-21"
   last_start_date <- "2020-01-21"
   day_step <- 1
-  
+
   scan_results = scan_beta_date(
     min_beta = min_beta,
     max_beta = max_beta,
     beta_step = beta_step,
-    first_start_date = first_start_date, 
-    last_start_date = last_start_date, 
+    first_start_date = first_start_date,
+    last_start_date = last_start_date,
     day_step = day_step,
     data = data)
-  
+
   # No transmission b = 0 much less likely than some transmission b = 0.1
   expect_lt(scan_results$renorm_mat_LL[[1]], scan_results$renorm_mat_LL[[2]])
-  
+
 })
 
 test_that("Unreasonable start dates are less likely", {
   set.seed(1)
-  
+
   data <- read.csv(sircovid_file("extdata/example.csv"),
                    stringsAsFactors = FALSE)
-  
+
   # Parameters for run
   min_beta <- 0.1
   max_beta <- 0.1
@@ -127,26 +150,26 @@ test_that("Unreasonable start dates are less likely", {
   first_start_date <- "2020-01-01"
   last_start_date <- "2020-02-29"
   day_step <- 20
-  
+
   scan_results = scan_beta_date(
     min_beta = min_beta,
     max_beta = max_beta,
     beta_step = beta_step,
-    first_start_date = first_start_date, 
-    last_start_date = last_start_date, 
+    first_start_date = first_start_date,
+    last_start_date = last_start_date,
     day_step = day_step,
     data = data)
-  
+
   # Mid Jan start most likely
   expect_gt(scan_results$renorm_mat_LL[[2]], scan_results$renorm_mat_LL[[1]])
   expect_gt(scan_results$renorm_mat_LL[[2]], scan_results$renorm_mat_LL[[3]])
-  
+
 })
 
 test_that("Bad parameters create errors", {
   data <- read.csv(sircovid_file("extdata/example.csv"),
                    stringsAsFactors = FALSE)
-  
+
   # Parameters for run
   min_beta <- 0.1
   max_beta <- 0.1
@@ -154,7 +177,7 @@ test_that("Bad parameters create errors", {
   first_start_date <- "2020-01-01"
   last_start_date <- "2020-02-29"
   day_step <- 20
-  
+
   sircovid_model <- basic_model(progression_groups = list(E = 2, asympt = 1, mild = 1, ILI = 1, hosp = 2, ICU = 2, rec = 2),
                                 gammas = list(E = 1/2.5, asympt = 1/2.09, mild = 1/2.09, ILI = 1/4, hosp = 2/1, ICU = 2/5, rec = 2/5))
   model_params <- generate_parameters(
@@ -167,18 +190,18 @@ test_that("Bad parameters create errors", {
     trans_profile = 1,
     trans_increase = 1,
     dt = 0.25)
-  
+
   expect_error(scan_beta_date(
     min_beta = min_beta,
     max_beta = max_beta,
     model_params = model_params,
     beta_step = beta_step,
-    first_start_date = first_start_date, 
-    last_start_date = last_start_date, 
+    first_start_date = first_start_date,
+    last_start_date = last_start_date,
     day_step = day_step,
     data = data),
     "Set beta variation through generate_beta_func in sircovid_model, not model_params")
-  
+
   model_params <- generate_parameters(
     sircovid_model = sircovid_model,
     transmission_model = "POLYMOD",
@@ -189,14 +212,14 @@ test_that("Bad parameters create errors", {
     trans_profile = 1,
     trans_increase = 1,
     dt = 0.25)
-  
+
   expect_error(scan_beta_date(
     min_beta = min_beta,
     max_beta = max_beta,
     model_params = model_params,
     beta_step = beta_step,
-    first_start_date = first_start_date, 
-    last_start_date = last_start_date, 
+    first_start_date = first_start_date,
+    last_start_date = last_start_date,
     day_step = day_step,
     data = data,
     scale_prior = 36),
