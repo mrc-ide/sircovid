8522ee2cbe8125768879337289c28c5b5ead01b0
edknock <e.knock@imperial.ac.uk>
2020-04-24 12:03:28 +0100

    switch to truncated poisson function

--- R/seeding.R
+++ R/seeding.R
@@ -7,9 +7,9 @@
 ##' @param data The data, must be constructed with
 ##'   \code{particle_filter_data}
 ##'
-##' @param pars_seeding Parameters for seeding
+##' @param pars_seeding A list of parameters used for seeding 
 ##' 
-##' @importFrom stats rpois
+##' @importFrom extraDistr rtpois
 ##' 
 ##' @export
 seeding_function <- function(model,
@@ -18,24 +18,25 @@ seeding_function <- function(model,
 ){
   
   steps_per_day <- attr(data, "steps_per_day")
+  min_seeding_step <- data$step_start[1L] #earliest possible seeding time
+  max_seeding_step <- data$step_end[1L] #latest possible seeding time (should be 1 day before first_data_step)
+  first_data_step <- data$step_end[2L] #first data point time
   
   function(n_particles){
     if (is.null(pars_seeding)){
     
       #if pars_seeding is NULL, then we seed all particles at start_date with same state  
-      step <- rep(data$step_start[1L],n_particles)
+      step <- rep(min_seeding_step,n_particles)
       initial_state <- model$initial()
       state <- array(initial_state,dim=c(length(initial_state),n_particles))
     
     } else {
     
-      #variable seeding time  
-      step <- data$step_end[1L] - rpois(n_particles,pars_seeding$lambda*steps_per_day)
-      while (any(step < data$step_start[1L])){
-        #make sure no seeding time is before start_date
-        k <- which(step < data$step_start[1L])
-        step[k] <- data$step_end[1L] - rpois(length(k),pars_seeding$lambda*steps_per_day)
-      }
+      #variable seeding time
+      #sample a poisson number of time steps before the first data time point. Truncated such that
+      #min_seeding_step <= step <= max_seeding_step
+      step <- first_data_step - rtpois(n = n_particles, lambda = pars_seeding$lambda*steps_per_day,
+                                       a = first_data_step - max_seeding_step - 1, b = first_data_step - min_seeding_step)
     
       #this does the same as when pars_seeding is NULL, but the seeding state could also be varied between particles
       initial_state <- model$initial()
