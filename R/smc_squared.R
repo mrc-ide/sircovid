3fb7943ab1b4da795fbbc87bcaa6e07faf94e4ca
edknock <e.knock@imperial.ac.uk>
2020-04-30 18:09:11 +0100

    output ESS

--- R/smc_squared.R
+++ R/smc_squared.R
@@ -8,7 +8,7 @@
 ##' 
 ##' @param model_params Default set of parameters used in odin_model
 ##' 
-##' @param beta_params List of parameters used for time-varying beta
+##' @param beta_params List of default parameters used for time-varying beta
 ##' 
 ##' @param pars_obs Observation parameters
 ##' 
@@ -20,9 +20,9 @@
 ##'
 ##' @param n_param_sets Number of parameter sets
 ##'
-##' @param fitted_params list of fitted parameters, with a list of 
-##' log-density function for prior ($dprior), random generator for 
-##' prior ($rprior) and random generator used for proposal ($rproposal)
+##' @param fitted_params list of which parameters are being fitted,
+##' with for each a list of log-density function for prior ($dprior)
+##' and random generator for prior ($rprior)
 ##' 
 ##' @param degeneracy_threshold A number in (0,1), threshold used to 
 ##' determine whether or not to do an MCMC move for the parameters 
@@ -31,10 +31,18 @@
 ##' multivariate normal proposal of parameters
 ##'
 ##' @export
-smc_squared <- function(data, sircovid_model, model_params, beta_params,
-                        pars_obs, pars_seeding, earliest_seeding_date, 
-                        n_particles, n_param_sets, fitted_params,
-                        degeneracy_threshold, covariance_scaling) {
+smc_squared <- function(data,
+                        sircovid_model,
+                        model_params,
+                        beta_params,
+                        pars_obs,
+                        pars_seeding,
+                        earliest_seeding_date, 
+                        n_particles,
+                        n_param_sets,
+                        fitted_params,
+                        degeneracy_threshold = 0.5,
+                        covariance_scaling = 0.5) {
 
   if (n_particles < 2) {
     stop("At least two particles required")
@@ -57,9 +65,11 @@ smc_squared <- function(data, sircovid_model, model_params, beta_params,
   
   ####Calculate log_prior for all parameter sets in params
   log_prior <- calc_log_prior(params,fitted_params)
-    
-  ## Special treatment for the burn-in phase; later we might use this
-  ## same approach for skipping steps though.
+  
+  # create progress bar
+  pb <- txtProgressBar(min = 0, max = nrow(data), style = 3)
+  setTxtProgressBar(pb, 0)
+  ## Initialise the particles for each parameter set and run up to the data
   smc_sq_pre_data_run <- furrr::future_pmap(
     .l = list(k=seq_len(n_param_sets)), .f = smc_sq_pre_data_run_model,
     params = params, data = data, sircovid_model = sircovid_model,
@@ -71,14 +81,18 @@ smc_squared <- function(data, sircovid_model, model_params, beta_params,
   
   state <- array(unlist(smc_sq_pre_data_run), dim = c(length(i_state),n_particles,n_param_sets))
   
+  #initialise log likelihood and log weights for each parameter set
   log_likelihood <- rep(0,n_param_sets)
   log_param_weights <- rep(0,n_param_sets)
   
+  ESS_t <- rep(0,nrow(data))
+  
   for (t in seq_len(nrow(data))[-1L]) {
-    
+    setTxtProgressBar(pb, t-1)
     step <- c(data$step_start[t], data$step_end[t])
     prev_state <- state
     
+    ##run the model forward for all the particles of each parameter set
     model_run <- furrr::future_pmap(
       .l = list(k=seq_len(n_param_sets)), .f = smc_sq_run_model_resample,
       t = t, data = data, sircovid_model = sircovid_model,
@@ -87,11 +101,11 @@ smc_squared <- function(data, sircovid_model, model_params, beta_params,
       pars_obs = pars_obs, step = step, earliest_seeding_date = earliest_seeding_date
     )
     
+    state <- array(unlist(furrr::future_map(model_run, ~ .$state)), dim = dim(state))
+    #update log likelihood and log weights for the parameter sets
     log_ave_weight <- furrr::future_map_dbl(model_run, ~ .$log_ave_weight)
     log_likelihood <- log_likelihood + log_ave_weight
     log_param_weights <- log_param_weights + log_ave_weight
-    state <- array(unlist(furrr::future_map(model_run, ~ .$state)), dim = dim(state))
-
     
     #param_weights are the weights for each parameter set
     param_weights<-scale_log_weights(log_param_weights)
@@ -103,7 +117,7 @@ smc_squared <- function(data, sircovid_model, model_params, beta_params,
     #Calculate Effective Sample Size (squared-sum of weights/sum of squared-weights)
     ESS <- sum(param_weights$weights)^2/sum(param_weights$weights^2)
     
-    #MCMC update to parameters only if ESS is below a threshold
+    #Resample and MCMC move parameters only if ESS is below a threshold
     if (ESS < degeneracy_threshold * n_param_sets){
       
       #calculate weighted covariance of the fitted parameters
@@ -154,9 +168,11 @@ smc_squared <- function(data, sircovid_model, model_params, beta_params,
       #reset weights for parameter sets (note that we do not reset when ESS is above threshold)
       log_param_weights[] <- 0
     }
-    
+    ESS_t[t]<-ESS
   }
 
+  close(pb)
+  attr(params,'ESS') <- ESS_t
   params
 }
 
