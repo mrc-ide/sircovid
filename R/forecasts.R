f006dacb2f7c062175bb9d4b47500a85ad892210
John Lees <lees.john6@gmail.com>
2020-05-05 12:45:44 +0100

    Version of pmcmc sample like sample_grid

--- R/forecasts.R
+++ R/forecasts.R
@@ -61,7 +61,6 @@ sample_grid_scan <- function(scan_results,
   pars_obs <- scan_results$inputs$pars_obs
   data <- scan_results$inputs$data
   
-  
   # Multi-core futures with furrr (parallel purrr)
   
   ## Particle filter outputs
@@ -74,37 +73,80 @@ sample_grid_scan <- function(scan_results,
     forecast_days = forecast_days, 
     save_particles = TRUE, return = "sample")
 
-  # collapse into an array of trajectories
-  # the trajectories are different lengths in terms of dates
-  # so we will fill the arrays with NAs where needed
-  num_rows <- unlist(lapply(traces, nrow))
-  max_rows <- max(num_rows)
-  seq_max <- seq_len(max_rows)
-  max_date_names <- rownames(traces[[which.max(unlist(lapply(traces, nrow)))]])
+  trajectories <- traces_to_trajectories(traces
   
-  trajectories <- array(NA, 
-                        dim = c(max_rows, ncol(traces[[1]]), length(traces)),
-                        dimnames = list(max_date_names, NULL, NULL))
+  class(res) <- "sircovid_forecast"
+  return(res)
   
-  # fill the tail of the array slice
-  # This is so that the end of the trajectories array is populated, 
-  # and the start is padded with NA if it's shorter than the max. 
-  for (i in seq_len(length(traces))){
-    trajectories[tail(seq_max, nrow(traces[[i]])), , i] <- traces[[i]]
+}
+
+
+##' Take a parameter search produced by \code{\link{pmcmc}} and 
+##' sample \code{n_sample_pairs} from the parameter space
+##' on their probability. For each parameter pair chosen, run particle
+##' filter with \code{num_particles} and sample 1 trajectory
+##' 
+##' @title Sample pmcmc
+##' 
+##' @param mcmc_results Output of \code{\link{pmcmc}}.
+##' 
+##' @param n_sample_pairs Number of parameter pairs to be sampled. This will 
+##'   determine how many trajectories are returned. Integer. Default = 10. This 
+##'   will determine how many trajectories are returned. 
+##'   
+##' @param n_particles Number of particles. Positive Integer. Default = 100
+##' 
+##' @param forecast_days Number of days being forecast. Default = 0
+##'   
+##' @return \code{\link{list}}. First element (trajectories) is a 3 
+##'   dimensional array of trajectories (time, state, tranjectories). Second 
+##'   element (param_grid) is the parameters chosen when  sampling from the 
+##'   \code{scan_results} grid and the third dimension (inputs) is a list of
+##'   model inputs. 
+##' 
+##' @export
+##' @import furrr
+sample_pmcmc <- function(mcmc_results,
+                         burn_in = 101,
+                         n_sample_pairs = 10, 
+                         n_particles = 100, 
+                         forecast_days = 0) {
+  
+  # checks on args
+  assert_is(scan_results, "pmcmc_list")
+  assert_pos_int(n_sample_pairs)
+  assert_pos_int(n_particles)
+  assert_pos_int(forecast_days)
+  
+  # discard 10% as burn-in
+  pars_to_sample <- names(mcmc_results$inputs$pars$pars_init)
+  chains <- create_master_chain(mcmc_results, burn_in)$results
+  param_grid <- chains[sample.int(n = nrow(chains), 
+                                  size  = n_sample_pairs, 
+                                  replace = FALSE), pars_to_sample]
+  
+  forecasts <- function(sampled_pars) {
+      pars <- as.list(sampled_pars)
+      trace <- calc_loglikelihood(pars, 
+              mcmc_results$inputs$data, 
+              mcmc_results$inputs$sircovid_model, 
+              mcmc_results$inputs$model_params,
+              mcmc_results$inputs$steps_per_day, 
+              mcmc_results$inputs$pars_obs, 
+              n_particles,
+              forecast_days,
+              return = "full")
+      trace
   }
 
-  # combine and return
-  res <- list("trajectories" = trajectories,
-              "param_grid" = param_grid,
-              inputs = list(
-                model_params = model_params,
-                pars_obs = pars_obs,
-                data = data,
-                model = sircovid_model,
-                forecast_days = forecast_days))
+  # Run forecasts in parallel
+  traces <- furrr::future_pmap(
+    .l = param_grid, 
+    .f = forecasts)
   
-  class(res) <- "sample_grid_search"
+  trajectories <- traces_to_trajectories(traces)
   
+  class(sample_res) <- "sircovid_forecast"
   return(res)
   
 }
@@ -123,7 +165,7 @@ sample_grid_scan <- function(scan_results,
 ##' @param quantiles Quantiles to summarise forecast variance.
 ##' 
 ##' @export
-summary.sample_grid_search <- function(object, ...,
+summary.sircovid_forecast <- function(object, ...,
                                        what = "deaths",
                                        quantiles = seq(from=0.05, to=0.95, by=0.05)) {
   totals <- sum_over_compartments(object)[[what]]
@@ -159,4 +201,30 @@ sum_over_compartments <- function(sample_grid_res) {
     totals$I_ICU_R + totals$I_ICU_D + totals$R_stepdown
 
   totals
+}
+
+##' collapse into an array of trajectories
+##' the trajectories are different lengths in terms of dates
+##' so we will fill the arrays with NAs where needed
+##' @importFrom utils tail
+##' @noRd
+traces_to_trajectories <- function(traces) {
+
+  num_rows <- unlist(lapply(traces, nrow))
+  max_rows <- max(num_rows)
+  seq_max <- seq_len(max_rows)
+  max_date_names <- rownames(traces[[which.max(unlist(lapply(traces, nrow)))]])
+  
+  trajectories <- array(NA, 
+                        dim = c(max_rows, ncol(traces[[1]]), length(traces)),
+                        dimnames = list(max_date_names, NULL, NULL))
+  
+  # fill the tail of the array slice
+  # This is so that the end of the trajectories array is populated, 
+  # and the start is padded with NA if it's shorter than the max. 
+  for (i in seq_len(length(traces))){
+    trajectories[tail(seq_max, nrow(traces[[i]])), , i] <- traces[[i]]
+  }
+
+  trajectories
 }
--- R/pmcmc.R
+++ R/pmcmc.R
@@ -254,7 +254,9 @@ pmcmc <- function(data,
                         model_params = model_params,
                         steps_per_day = steps_per_day, 
                         pars_obs = pars_obs, 
-                        n_particles = n_particles
+                        n_particles = n_particles,
+                        forecast_days = 0,
+                        return = "ll"
     ) 
     X
   }
@@ -495,9 +497,49 @@ run_mcmc_chain <- function(inputs,
 
 }
 
-
+##' Calculate log-likelihood
+##'
+##' @title Run odin model to calculate log-likelihood
+##' 
+##' @pars Parameters for MCMC
+##' 
+##' @param data Data to fit to.  This must be constructed with
+##'   \code{particle_filter_data}
+##'   
+##' @param sircovid_model An odin model generator and comparison function.
+##' 
+##' @param model_params Model parameters, from a call to
+##'   \code{generate_parameters()}. If NULL, uses defaults as
+##'   in unit tests.
+##'   
+##' @param steps_per_day Number of steps per day
+##'   
+##' @param pars_obs list of parameters to use in comparison
+##'   with \code{compare_icu}. If NULL, uses
+##'   list(phi_general = 0.95, k_general = 2,phi_ICU = 0.95, k_ICU = 2, phi_death = 926 / 1019, k_death = 2, exp_noise = 1e6)
+##'                   
+##' @param n_particles Number of particles
+##' 
+##' @param return Set to 'll' to return the log-likelihood (for MCMC) or to
+##'   'sample' for the particle trajectories (for forecasting)
+##'
+##' @return a double with the log-likelihood, or matrix of particle trajectories
+##'   
+##' @export
 calc_loglikelihood <- function(pars, data, sircovid_model, model_params,
-                               steps_per_day, pars_obs, n_particles) {
+                               steps_per_day, pars_obs, n_particles,
+                               forecast_days = 0, return = "ll") {
+  if (return == "full") {
+    save_particles <- TRUE
+    pf_return <- "sample"
+  } else if (return == "ll") {
+    save_particles <- FALSE
+    forecast_days <- 0
+    pf_return <- "single"
+  } else {
+    stop("Unknown return type to calc_loglikelihood")
+  }
+  
   # pars[['start_date']] argument is an integer reflecting the number of days between 
   # the model start date and the first date in the data
   if ('start_date' %in% names(pars)) {
