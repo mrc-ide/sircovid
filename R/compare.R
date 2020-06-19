4de859237df704755d9f131a6192f812e11a66f3
edknock <e.knock@imperial.ac.uk>
2020-06-19 11:05:42 +0100

    update compare and models for serology2

--- R/compare.R
+++ R/compare.R
@@ -18,33 +18,67 @@ compare_output <- function(model, pars_obs, data, type="sircovid_basic") {
   index <- odin_index(model)
 
   ## Unpack things that we will use repeatedly
-  phi_ICU <- pars_obs$phi_ICU
-  k_ICU <- pars_obs$k_ICU
-  phi_death <- pars_obs$phi_death
-  k_death <- pars_obs$k_death
+  if ("phi_ICU" %in% names(pars_obs)){
+    phi_ICU <- pars_obs$phi_ICU
+  }
+  if ("k_ICU" %in% names(pars_obs)){
+    k_ICU <- pars_obs$k_ICU
+  }
+  if ("phi_death" %in% names(pars_obs)){
+    phi_death <- pars_obs$phi_death
+  }
+  if ("k_death" %in% names(pars_obs)){
+    k_death <- pars_obs$k_death
+  }
+  if ("phi_death_hosp" %in% names(pars_obs)){
+    phi_death_hosp <- pars_obs$phi_death_hosp
+  }
+  if ("k_death_hosp" %in% names(pars_obs)){
+    k_death_hosp <- pars_obs$k_death_hosp
+  }
+  if ("phi_death_comm" %in% names(pars_obs)){
+    phi_death_comm <- pars_obs$phi_death_comm
+  }
+  if ("k_death_comm" %in% names(pars_obs)){
+    k_death_comm <- pars_obs$k_death_comm
+  }
+  if ("phi_general" %in% names(pars_obs)){
+    phi_general <- pars_obs$phi_general
+  }
+  if ("k_general" %in% names(pars_obs)){
+    k_general <- pars_obs$k_general
+  }
+  if ("phi_admitted" %in% names(pars_obs)){
+    phi_admitted <- pars_obs$phi_admitted
+  }
+  if ("k_admitted" %in% names(pars_obs)){
+    k_admitted <- pars_obs$k_admitted
+  }
+  if ("phi_new" %in% names(pars_obs)){
+    phi_new <- pars_obs$phi_new
+  }
+  if ("k_new" %in% names(pars_obs)){
+    k_new <- pars_obs$k_new
+  }
+  
   exp_noise <- pars_obs$exp_noise
+  
   if (type == "sircovid_basic") {
     index_ICU <- c(index$I_ICU) - 1L
     index_D <- c(index$D) - 1L
   } else if (type == "sircovid_hospital") {
-    phi_general <- pars_obs$phi_general
-    k_general <- pars_obs$k_general
     index_general <- c(c(index$I_triage),c(index$I_hosp_R),c(index$I_hosp_D),c(index$R_stepdown)) - 1L
     index_ICU <- c(c(index$I_ICU_R),c(index$I_ICU_D)) - 1L
     index_D <- c(index$D) - 1L
-  } else if (type == "sircovid_serology") {
-    phi_general <- pars_obs$phi_general
-    k_general <- pars_obs$k_general
-    phi_admitted <- pars_obs$phi_admitted
-    k_admitted <- pars_obs$k_admitted
-    phi_new <- pars_obs$phi_new
-    k_new <- pars_obs$k_new
+  } else if (type %in% c("sircovid_serology","sircovid_serology2")) {
     p_specificity <- pars_obs$p_specificity
     index_general <- c(c(index$I_triage_R_conf),c(index$I_triage_D_conf),c(index$I_hosp_R_conf),c(index$I_hosp_D_conf),c(index$R_stepdown_conf)) - 1L
     index_admit <- c(index$cum_admit_conf) - 1L
     index_new <- c(index$cum_new_conf) - 1L
     index_ICU <- c(c(index$I_ICU_R_conf),c(index$I_ICU_D_conf)) - 1L
     index_D <- c(index$D_hosp,c(index$D_comm)) - 1L
+    index_D_hosp <- c(index$D_hosp) - 1L
+    index_D_comm <- c(index$D_comm) - 1L
     index_R_pos <- c(index$R_pos) - 1L
     index_R_neg <- c(index$R_neg) - 1L
     index_R_pre <- index$R_pre - 1L #need to retain array structure for this
@@ -69,41 +103,68 @@ compare_output <- function(model, pars_obs, data, type="sircovid_basic") {
       ## sum model ITU cases output across ages/infectivities
       model_icu <- colSums(state[index_ICU, ])
       log_weights <- log_weights +
-        ll_nbinom(data$itu[t], model_icu, phi_ICU, k_ICU, exp_noise)
+        ll_nbinom(data$itu[t], phi_ICU * model_icu, k_ICU, exp_noise)
     }
     
-    if (type %in% c("sircovid_hospital","sircovid_serology")  && !is.na(data$general[t])) {
+    if (type %in% c("sircovid_hospital","sircovid_serology","sircovid_serology2")  && !is.na(data$general[t])) {
       ## sum model output across ages/infectivities
       model_general <- colSums(state[index_general, ])
       log_weights <- log_weights +
-        ll_nbinom(data$general[t], model_general, phi_general, k_general, exp_noise)
-    }
-
-    if (!is.na(data$deaths[t])) {
-      ## new deaths summed across ages/infectivities
-      model_deaths <- colSums(state[index_D, ]) -
-        colSums(prev_state[index_D, ])
-      log_weights <- log_weights +
-        ll_nbinom(data$deaths[t], model_deaths, phi_death, k_death, exp_noise)
+        ll_nbinom(data$general[t], phi_general * model_general, k_general, exp_noise)
     }
     
-    if (type %in% c("sircovid_serology")  && !is.na(data$admitted[t])) {
+    if (type!="sircovid_serology2"){
+      if (!is.na(data$deaths[t])) {
+        ## new deaths summed across ages/infectivities
+        model_deaths <- colSums(state[index_D, ]) -
+          colSums(prev_state[index_D, ])
+        log_weights <- log_weights +
+          ll_nbinom(data$deaths[t], phi_death * model_deaths, k_death, exp_noise)
+      }
+    } else {
+      if(!is.na(data$deaths_hosp[t]) || !is.na(data$deaths_comm[t])){
+        if (!is.na(data$deaths_hosp[t])) {
+          ## new deaths summed across ages/infectivities
+          model_deaths_hosp <- colSums(state[index_D_hosp, ]) -
+            colSums(prev_state[index_D_hosp, ])
+          log_weights <- log_weights +
+            ll_nbinom(data$deaths_hosp[t], phi_death_hosp * model_deaths_hosp, k_death_hosp, exp_noise)
+        }
+        if (!is.na(data$deaths_comm[t])) {
+          ## new deaths summed across ages/infectivities
+          model_deaths_comm <- colSums(state[index_D_comm, ]) -
+            colSums(prev_state[index_D_comm, ])
+          log_weights <- log_weights +
+            ll_nbinom(data$deaths_comm[t], phi_death_comm * model_deaths_comm, k_death_comm, exp_noise)
+        }
+      } else if (!is.na(data$deaths[t])) {
+          ## new deaths summed across ages/infectivities
+          model_deaths_hosp <- colSums(state[index_D_hosp, ]) -
+            colSums(prev_state[index_D_hosp, ])
+          model_deaths_comm <- colSums(state[index_D_comm, ]) -
+            colSums(prev_state[index_D_comm, ])
+          log_weights <- log_weights +
+            ll_nbinom(data$deaths[t], phi_death_hosp * model_deaths_hosp + phi_death_comm * model_deaths_comm, k_death, exp_noise)
+      }
+    }
+    
+    if (type %in% c("sircovid_serology","sircovid_serology2")  && !is.na(data$admitted[t])) {
       ## sum model output across ages/infectivities
       model_admitted <- colSums(state[index_admit, ,drop = FALSE]) -
         colSums(prev_state[index_admit, ,drop = FALSE])
       log_weights <- log_weights +
-        ll_nbinom(data$admitted[t], model_admitted, phi_admitted, k_admitted, exp_noise)
+        ll_nbinom(data$admitted[t], phi_admitted * model_admitted, k_admitted, exp_noise)
     }
     
-    if (type %in% c("sircovid_serology")  && !is.na(data$new[t])) {
+    if (type %in% c("sircovid_serology","sircovid_serology2")  && !is.na(data$new[t])) {
       ## sum model output across ages/infectivities
       model_new <- colSums(state[index_new, ,drop = FALSE]) -
         colSums(prev_state[index_new, ,drop = FALSE])
       log_weights <- log_weights +
-        ll_nbinom(data$new[t], model_new, phi_new, k_new, exp_noise)
+        ll_nbinom(data$new[t], phi_new * model_new, k_new, exp_noise)
     }
     
-    if (type %in% c("sircovid_serology")  && !is.na(data$ntot_0_14[t]) && !is.na(data$npos_0_14[t])) {
+    if (type %in% c("sircovid_serology","sircovid_serology2")  && !is.na(data$ntot_0_14[t]) && !is.na(data$npos_0_14[t])) {
       agegroups <- seq.int(1,3)
       prob_true_pos <- colSums(state[index_R_pos[agegroups], ,drop = FALSE]) / sum(N_tot[agegroups])
       prob_false_pos <- (1 - p_specificity) * (1 - colSums(state[c(index_R_pos[agegroups], index_R_neg[agegroups], c(index_R_pre[agegroups,])), ,drop = FALSE]) / sum(N_tot[agegroups]))
@@ -112,7 +173,7 @@ compare_output <- function(model, pars_obs, data, type="sircovid_basic") {
         dbinom(data$npos_0_14[t], size = data$ntot_0_14[t], prob = prob_true_pos + prob_false_pos, log = TRUE)
     }
     
-    if (type %in% c("sircovid_serology")  && !is.na(data$ntot_15_64[t]) && !is.na(data$npos_15_64[t])) {
+    if (type %in% c("sircovid_serology","sircovid_serology2")  && !is.na(data$ntot_15_64[t]) && !is.na(data$npos_15_64[t])) {
       agegroups <- seq.int(4,13)
       prob_true_pos <- colSums(state[index_R_pos[agegroups], ,drop = FALSE]) / sum(N_tot[agegroups])
       prob_false_pos <- (1 - p_specificity) * (1 - colSums(state[c(index_R_pos[agegroups], index_R_neg[agegroups], c(index_R_pre[agegroups,])), ,drop = FALSE]) / sum(N_tot[agegroups]))
@@ -121,7 +182,7 @@ compare_output <- function(model, pars_obs, data, type="sircovid_basic") {
         dbinom(data$npos_15_64[t], size = data$ntot_15_64[t], prob = prob_true_pos + prob_false_pos, log = TRUE)
     }
     
-    if (type %in% c("sircovid_serology")  && !is.na(data$ntot_65plus[t]) && !is.na(data$npos_65plus[t])) {
+    if (type %in% c("sircovid_serology","sircovid_serology2")  && !is.na(data$ntot_65plus[t]) && !is.na(data$npos_65plus[t])) {
       agegroups <- seq.int(14,17)
       prob_true_pos <- colSums(state[index_R_pos[agegroups], ,drop = FALSE]) / sum(N_tot[agegroups])
       prob_false_pos <- (1 - p_specificity) * (1 - colSums(state[c(index_R_pos[agegroups], index_R_neg[agegroups], c(index_R_pre[agegroups,])), ,drop = FALSE]) / sum(N_tot[agegroups])) 
@@ -132,4 +193,10 @@ compare_output <- function(model, pars_obs, data, type="sircovid_basic") {
     
     log_weights
   }
+}
+
+##' @importFrom stats dnbinom rexp
+ll_nbinom <- function(data, model, k, exp_noise) {
+  mu <- model + rexp(length(model), rate = exp_noise)
+  dnbinom(data, k, mu = mu, log = TRUE)
 }
