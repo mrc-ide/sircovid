766030eb5b44d43436d96807fc9f893c6cdd0af7
edknock <e.knock@imperial.ac.uk>
2020-07-16 12:20:40 +0100

    test for piecewise linear beta

--- tests/testthat/test-beta.R
+++ tests/testthat/test-beta.R
@@ -107,6 +107,42 @@ test_that("Three-level beta works in odin as expected", {
   
 })
 
+test_that("Piecewise-linear beta works in odin as expected", {
+  
+  sircovid_model <- hospital_model()
+  sircovid_model$generate_beta_func <- generate_beta_piecewise_linear
+  
+  beta = sircovid_model$generate_beta_func(beta_k = c(3,
+                                                      1.5,
+                                                      1.8,
+                                                      1.2),
+                                           t_k = sircovid_date(c("2020-03-16",
+                                                                 "2020-03-25",
+                                                                 "2020-05-11",
+                                                                 "2020-06-03")),
+                                           start_date = sircovid_date("2020-02-06"),
+                                           dt = 0.25)
+  pars_model = generate_parameters(sircovid_model = sircovid_model,
+                                   beta = beta$beta,
+                                   beta_times = beta$beta_times)
+  mod <- sircovid_model$odin_model(user = pars_model)
+  
+  ## As in the old version:
+  pars_model$beta_t <- normalise_beta(beta$beta_times, pars_model$dt)
+  pars_model$beta_y <- beta$beta
+  
+  t_max <- max(pars_model$beta_t)+50
+  t <- seq(from = 1, to = t_max)
+  tmp <- mod$run(t)
+  results <- mod$transform_variables(tmp)
+  
+  expect_equal(results$beta,pars_model$beta_y[sapply(t,FUN=function(t){
+    max(which(pars_model$beta_t<=t))
+  })])
+  
+  
+})
+
 test_that("Three-level beta works the same with generate_beta/generate_parameters as with update_beta", {
   
   sircovid_model <- hospital_model()
@@ -126,6 +162,42 @@ test_that("Three-level beta works the same with generate_beta/generate_parameter
                              beta_pl = 1.8,
                              start_date = sircovid_date("2020-02-06"),
                              dt = 0.25)
+  
+  expect_equal(pars_model$beta_step,beta_update$beta_step)
+  
+})
+
+test_that("Piecewise-linear beta works the same with generate_beta/generate_parameters as with update_beta", {
+  
+  sircovid_model <- hospital_model()
+  sircovid_model$generate_beta_func <- generate_beta_piecewise_linear
+  
+  beta = sircovid_model$generate_beta_func(beta_k = c(3,
+                                                      1.5,
+                                                      1.8,
+                                                      1.2),
+                                           t_k = sircovid_date(c("2020-03-16",
+                                                                 "2020-03-25",
+                                                                 "2020-05-11",
+                                                                 "2020-06-03")),
+                                           start_date = sircovid_date("2020-02-06"),
+                                           dt = 0.25)
+  pars_model = generate_parameters(sircovid_model = sircovid_model,
+                                   beta = beta$beta,
+                                   beta_times = beta$beta_times,
+                                   dt = 0.25)
+  
+  beta_update <- update_beta_piecewise_linear(sircovid_model = sircovid_model, 
+                                              beta_k = c(3,
+                                                         1.5,
+                                                         1.8,
+                                                         1.2),
+                                              t_k = sircovid_date(c("2020-03-16",
+                                                                    "2020-03-25",
+                                                                    "2020-05-11",
+                                                                    "2020-06-03")),
+                                              start_date = sircovid_date("2020-02-06"),
+                                              dt = 0.25)
   
   expect_equal(pars_model$beta_step,beta_update$beta_step)
   
