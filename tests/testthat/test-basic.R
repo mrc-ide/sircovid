9a3b4e70eaa32111cf24378ee9b3a5906a86daba
Rich FitzJohn <rich.fitzjohn@gmail.com>
2020-07-31 20:00:12 +0100

    Remove initial tests

--- tests/testthat/test-basic.R
+++ tests/testthat/test-basic.R
@@ -1,17 +1,5 @@
 context("basic")
 
-## TODO: this test should come out and be replaced by something that
-## does not load black-box parameters. This version just runs the model
-test_that("can run the basic model", {
-  d <- readRDS("basic.rds")
-  mod <- basic$new(d$user, 0, 10)
-  expect_equal(names(mod$index()), names(d$index)[-1])
-  mod$set_index(integer(0))
-  res <- mod$run(300)
-  expect_equal(res, matrix(numeric(0), 0, 10))
-})
-
-
 test_that("can run the basic model", {
   p <- basic_parameters(sircovid_date("2020-02-07"), "england")
   mod <- basic$new(p, 0, 10)
