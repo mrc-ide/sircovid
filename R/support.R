b7883f95ce24221312ae14009c16cac5e9e3eb6b
Rich FitzJohn <rich.fitzjohn@gmail.com>
2020-08-20 09:42:02 +0100

    Export more date functions

--- R/support.R
+++ R/support.R
@@ -21,54 +21,6 @@ check_age_bins <- function(age_headers) {
 }
 
 
-##' Convert a date into the number of days into (or past) 2020
-##'
-##' @title Convert date to number
-##'
-##' @param date A Date object, or something that can be converted to one
-##'
-##' @return An integer, being the number of days into 2020
-##' @export
-##' @examples
-##' sircovid_date("2020-01-01")
-##' sircovid_date(c("2020-03-01", "2020-10-01"))
-sircovid_date <- function(date) {
-  days_into_2020 <- as.numeric(as_date(date) - as_date("2019-12-31"))
-  if (any(days_into_2020 < 0)) {
-    stop("Negative dates, sircovid_date likely applied twice")
-  }
-  days_into_2020
-}
-
-
-sircovid_date_as_date <- function(date) {
-  assert_sircovid_date(date)
-  as_date("2019-12-31") + date
-}
-
-
-assert_sircovid_date <- function(date) {
-  if (!is.numeric(date)) {
-    stop("'date' must be numeric - did you forget sircovid_date()?")
-  }
-  if (any(date < 0)) {
-    stop("Negative dates, sircovid_date likely applied twice")
-  }
-  date
-}
-
-
-as_sircovid_date <- function(date) {
-  if (is.character(date)) {
-    sircovid_date(as_date(date))
-  } else if (is_date(date)) {
-    sircovid_date(date)
-  } else {
-    assert_sircovid_date(date)
-  }
-}
-
-
 sircovid_population <- function(region) {
   if (is.null(region)) {
     stop("'region' must not be NULL")
