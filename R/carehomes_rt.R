ad75e6bd3c25913518928e915df1d2c18a2f07a2
edknock <e.knock@imperial.ac.uk>
2020-09-18 18:13:56 +0100

    bump version number and update Rt calculation for beta not applied to carehomes

--- R/carehomes_rt.R
+++ R/carehomes_rt.R
@@ -28,7 +28,13 @@ carehomes_Rt <- function(step, S, p) {
 
   calculate_ev <- function(t, S, drop_carehomes) {
     ## Next-Generation-Matrix
-    ngm <- beta[t] * outer(mean_duration[, t], S[, t]) * p$m
+    m <- p$m
+    ages <- seq_len(p$N_age - 2)
+    ch <- seq(to = 19, length.out = 2)
+    m[ages, c(ages, ch)] <- beta[t] * m[ages, c(ages, ch)]
+    m[ch, ages] <- beta[t] * m[ch, ages]
+
+    ngm <- outer(mean_duration[, t], S[, t]) * m
 
     ## Care home workers (CHW) and residents (CHR) in last two rows
     ## and columns
