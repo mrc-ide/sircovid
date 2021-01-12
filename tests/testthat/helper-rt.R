97569134b4f13f3ccce8eba450ba77af665f07a4
edknock <e.knock@imperial.ac.uk>
2021-01-12 13:45:30 +0000

    Revert "update helper"
    This reverts commit f9566469df4afcb56fcafc0ecd4be743eab87c28.

--- tests/testthat/helper-rt.R
+++ tests/testthat/helper-rt.R
@@ -8,23 +8,17 @@ reference_data_rt <- function() {
     initial <- carehomes_initial(mod$info(), 10, p)
     mod$set_state(initial$state, initial$step)
     mod$set_index(integer(0))
-    index_S <- mod$info()$index$S
-    index_prob_strain <- mod$info()$index$prob_strain
-    index <- c(index_S, index_prob_strain)
-    
+    index <- mod$info()$index$S
+
     end <- sircovid_date("2020-05-01") / p$dt
     steps <- seq(initial$step, end, by = 1 / p$dt)
 
     set.seed(1)
-    x <- dust::dust_iterate(mod, steps)
-    
-    y <- x[index_S, , ]
-    prob_strain <- x[index_prob_strain, , ]
-    
-    rt_1 <- carehomes_Rt(steps, y[, 1, ], prob_strain[, 1, ], p)
-    rt_all <- carehomes_Rt_trajectories(steps, y, prob_strain, p)
+    y <- dust::dust_iterate(mod, steps, index)
+    rt_1 <- carehomes_Rt(steps, y[, 1, ], p)
+    rt_all <- carehomes_Rt_trajectories(steps, y, p)
 
-    list(inputs = list(steps = steps, y = y, prob_strain = prob_strain, p = p),
+    list(inputs = list(steps = steps, y = y, p = p),
          outputs = list(rt_1 = rt_1, rt_all = rt_all))
   })
 }
