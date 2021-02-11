25af3e9860ba4de7fde29095077e9ebf3bbb9709
edknock <e.knock@imperial.ac.uk>
2021-02-11 17:05:30 +0000

    fix test

--- R/carehomes.R
+++ R/carehomes.R
@@ -398,7 +398,7 @@ carehomes_parameters <- function(start_date, region,
 
   ## Proportion of population with covid-like symptoms without covid
   ret$p_NC <- p_NC
-  
+
   ## relative transmissibility of various I compartments
   ret$I_A_transmission <- 1
   ret$I_P_transmission <- 1
