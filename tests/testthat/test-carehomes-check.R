c429879d6b41a2ef62df16fdeda11af4e816f82d
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2021-03-08 09:00:55 +0000

    Remove stochasticity in test

--- tests/testthat/test-carehomes-check.R
+++ tests/testthat/test-carehomes-check.R
@@ -628,6 +628,7 @@ test_that("setting a gamma to 0 results in no progression", {
 
 
 test_that("No one is unconfirmed, if p_star = 1", {
+  set.seed(1)
   ## waning_rate default is 0, setting to a non-zero value so that this test
   ## passes with waning immunity
   p <- carehomes_parameters(0, "england", waning_rate = 1 / 20)
