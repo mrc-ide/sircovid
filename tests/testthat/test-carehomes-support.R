c0a277d244c7d450024b17443950dcb52afa334b
pabloperguz <p.perez-guzman@imperial.ac.uk>
2021-04-08 16:54:24 +0100

    get trajectories for deaths by age, place and vaccination class

--- tests/testthat/test-carehomes-support.R
+++ tests/testthat/test-carehomes-support.R
@@ -498,6 +498,7 @@ test_that("carehomes_index returns S compartments", {
       info$index$S,
       info$index$cum_admit_by_age,
       info$index$D_hosp,
+      info$index$D_non_hosp,
       info$index$I_weighted,
       info$index$prob_strain,
       info$index$cum_n_vaccinated))
