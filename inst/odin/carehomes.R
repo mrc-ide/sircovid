067e6c03f58c6ac4aec65d8783e5fe08a1344a59
edknock <e.knock@imperial.ac.uk>
2021-06-24 16:00:49 +0100

    fix so it works properly when n_real_strains is 1

--- inst/odin/carehomes.R
+++ inst/odin/carehomes.R
@@ -1617,7 +1617,7 @@ I_weighted_strain[, , ] <-
 update(I_weighted[, ]) <- sum(I_weighted_strain[i, , j])
 
 ## prob_strain is proportion of total I_weighted_strain in each strain
-prob_strain_1 <-
+prob_strain_1 <- if (n_real_strains == 1) 1 else
   (sum(I_weighted_strain[, 1, ]) + sum(I_weighted_strain[, 4, ])) /
   sum(I_weighted_strain)
 initial(prob_strain[1:n_real_strains]) <- 0
