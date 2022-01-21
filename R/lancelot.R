3c49fea1d2735beaa0175fc46294535444c316a7
Yasin Elmaci <76499702+epiyasin@users.noreply.github.com>
2022-01-21 12:21:53 +0000

    add deaths

--- R/lancelot.R
+++ R/lancelot.R
@@ -804,6 +804,7 @@ lancelot_index <- function(info, rt = FALSE, cum_admit = FALSE,
 
   ## Variables that we want to save for post-processing
   index_save <- c(hosp = index[["hosp_tot"]],
+                  deaths = index[["D_tot"]],
                   infections = index[["cum_infections"]],
                   infections_inc = index[["infections_inc"]])
   suffix <- paste0("_", c(sircovid_age_bins()$start, "CHW", "CHR"))
