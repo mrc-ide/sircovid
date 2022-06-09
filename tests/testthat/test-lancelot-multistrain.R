7fa803fa90d309ecb26fd48394c65ffbd3ad9dc4
edknock <edwardknock@gmail.com>
2022-06-09 17:39:14 +0100

    fix test

--- tests/testthat/test-lancelot-multistrain.R
+++ tests/testthat/test-lancelot-multistrain.R
@@ -1975,7 +1975,7 @@ test_that("strain_rel_gamma works as expected in lancelot_parameters", {
                "1 or 2")
   expect_error(lancelot_parameters(sircovid_date("2020-02-07"), "england",
                                    strain_transmission = c(1, 1),
-                                   strain_rel_gamma_A = c(2, 5)),
+                                   strain_rel_gamma_A = c(-1, 5)),
                "must be 1")
   expect_error(lancelot_parameters(sircovid_date("2020-02-07"), "england",
                                    strain_transmission = c(1, 1),
